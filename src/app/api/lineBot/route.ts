bbf33de4405bf8575eb8df9049339223b1bbaf80
kw_yeh <kw_yeh@cyberlink.com>
2023-06-27 11:15:41 +0800

    init

--- src/app/api/lineBot/route.ts
+++ src/app/api/lineBot/route.ts
@@ -1,5 +1,6 @@
-import {NextApiRequest, NextApiResponse} from "next";
+import {NextApiResponse} from "next";
 import {Client, WebhookEvent} from "@line/bot-sdk";
+import {NextRequest} from "next/server";
 
 const config = {
     channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN ?? 'YC+cfiwLguKTIdyYtO/96eHhMoCbVaVtn4mdfjVs+Uu7QvS+2xbP1xo1OxCqKEWNjG/0Z19fVtbg9ZBgmdLyCNF4ioPi72gQd9YN99qKToIG9y3rHXYj1wiAHUI2gc7ojwUMAxtFveNZN+AKSqNjFAdB04t89/1O/w1cDnyilFU=',
@@ -8,14 +9,8 @@ const config = {
 
 const client = new Client(config)
 
-interface reqBodyType extends NextApiRequest{
-    body: {
-        events: WebhookEvent[]
-    }
-}
-
-export async function POST(req: reqBodyType, res: NextApiResponse) {
-    const events: WebhookEvent[] = req.body.events
+export async function POST(req: NextRequest, res: NextApiResponse) {
+    const events: WebhookEvent[] = await req.json()
 
     try {
         await Promise.all(events.map(handleEvent))
