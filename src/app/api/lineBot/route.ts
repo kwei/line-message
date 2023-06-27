11a9c9a25b9a0ad27453b6957254cf12e2b4740e
kw_yeh <kw_yeh@cyberlink.com>
2023-06-27 13:56:48 +0800

    init

--- src/app/api/lineBot/route.ts
+++ src/app/api/lineBot/route.ts
@@ -13,19 +13,37 @@ export async function POST(req: Request, res: NextApiResponse) {
     console.log(1)
     const body2 = await req.body
     console.log(2, body2)
+    console.log(3, readStream(body2))
     // const events: WebhookEvent[] = bodyObj.events
     const events: WebhookEvent[] = []
-    console.log(3, events)
+    console.log(4, events)
     try {
         await Promise.all(events.map(handleEvent))
-        console.log(4)
+        console.log(5)
         return NextResponse.json({ status: 'OK' })
     } catch (e) {
-        console.log(5, e)
+        console.log(6, e)
         return NextResponse.json({ error: 'Internal Server Error: ' + e })
     }
 }
 
 async function handleEvent(event: WebhookEvent) {
     console.log(event)
+}
+
+async function readStream(stream: ReadableStream | null): Promise<string> {
+    if (!stream) return ''
+    const reader = stream.getReader()
+    let result = ''
+    const decoder = new TextDecoder()
+
+    while (true) {
+        const { done, value } = await reader.read()
+        if (done) break
+
+        const chunk = decoder.decode(value)
+        result += chunk
+    }
+
+    return result
 }
