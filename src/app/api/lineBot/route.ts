475a4352a776213175a18da88a9489c7ecbd7895
kw_yeh <kw_yeh@cyberlink.com>
2023-06-27 11:38:36 +0800

    init

--- src/app/api/lineBot/route.ts
+++ src/app/api/lineBot/route.ts
@@ -3,8 +3,8 @@ import {Client, WebhookEvent} from "@line/bot-sdk";
 import {NextRequest} from "next/server";
 
 const config = {
-    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN ?? 'YC+cfiwLguKTIdyYtO/96eHhMoCbVaVtn4mdfjVs+Uu7QvS+2xbP1xo1OxCqKEWNjG/0Z19fVtbg9ZBgmdLyCNF4ioPi72gQd9YN99qKToIG9y3rHXYj1wiAHUI2gc7ojwUMAxtFveNZN+AKSqNjFAdB04t89/1O/w1cDnyilFU=',
-    channelSecret: process.env.LINE_CHANNEL_SECRET ?? 'd3f86cb1d0d4b8f434f55a2cf7335683',
+    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN ?? '',
+    channelSecret: process.env.LINE_CHANNEL_SECRET ?? '',
 }
 
 const client = new Client(config)
